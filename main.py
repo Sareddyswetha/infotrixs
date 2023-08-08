9d37a0aa906a80f82d7a533d9f11c8949c8f0db2
Sareddyswetha <141572150+Sareddyswetha@users.noreply.github.com>
2023-08-08 22:53:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,60 @@
-def add_contact(contact,name,phone):
-    contact[name] = phone
-    print(f"{name} is added to contact")
-def search_contact(contact):
-    for i,j in contact.items():
-        print('Name:',i,'Number:',j)
-def update_contact(contact):
-    name = input('Enter the contact to be Edited')
-    if name in contact:
-        phone = input('Enter the new number')
-        contact[name] = phone
-        print('contact updated')
-        search_contact(contact)
+def add_contact(contacts):
+    name = input("Enter new contact name: ")
+    phone_no = input(f"Enter name's phone no: ")
+    contacts[name] = phone_no
+    print("New contact added!")
+
+
+def search_contact(contacts):
+    name = input("Enter the name to search for: ")
+    if name in contacts:
+        for i, j in contacts.items():
+            print("Name:", i, "Number:", j)
     else:
-        print('The name is not found')
-def delete_contact(contact, name):
-    if name in contact:
-        del contact[name]
-        print(f"{name} has been deleted from contact")
+        print("Contact not found.")
+
+
+def edit_contact(contacts):
+    name = input("Enter the contact name to edit: ")
+    if name in contacts:
+        phone_no = input(f"Enter new phone no for name: ")
+        contacts[name] = phone_no
+        print("Contact information updated!")
     else:
-        print(f"{name} not found in contact")
-def list_contact(contact):
-    if not contact:
-        print("No contact found")
+        print("Contact not found.")
+
+
+def delete_contact(contacts):
+    name = input("Enter the contact name to delete: ")
+    if name in contacts:
+        del contacts[name]
+        print("Contact deleted!")
     else:
-        print("Contact")
-        for name, contact in contact.items():
-            print('Name:', name, '\tNumber:', contact)
-def save_contact(contact, filename):
-    with open(filename, 'w') as file:
-        for name, contact in contact.items():
-            file.write(f"{name},{contact}\n")
-def load_contact(filename):
-    contact = {}
-    try:
-        with open(filename, 'r') as file:
-            lines = file.readlines()
-            for line in lines:
-                name, phone = line.strip().split(',')
-                contact[name] = phone
-    except FileNotFoundError:
-        pass
-    return contact
+        print("Contact not found.")
+
+
 def main():
-    filename = "contact.txt"
-    contacts = load_contact(filename)
+    contacts = {}
+    print("WELCOME\n")
     while True:
-        print("\nContact Management System")
-        print("1. Add Contact")
-        print("2. Update Contact")
-        print("3. Search Contact")
-        print("4. Delete Contact")
-        print("5. Save and Exit")
-        choice = input("Enter your choice (1-5):")
+        print("Menu\n1. Add new contact\n2. Search contact\n3. Edit contact\n4. Delete contact\n5. Exit")
+        choice = input("Enter your choice: ")
         if choice == '1':
-            name = input("Enter the name of the contact:")
-            phone = input("Enter the phone number:")
-            add_contact(contacts, name, phone)
+            add_contact(contacts)
         elif choice == '2':
-            update_contact(contacts)
+            search_contact(contacts)
         elif choice == '3':
-            Search_contact = input('enter the name')
-            if Search_contact in contacts:
-                print('Name :', Search_contact, '\nContact number:', contacts[Search_contact])
-            else:
-                print('The name is not found in contact book')
+            edit_contact(contacts)
         elif choice == '4':
-            name = input("Enter the name: ")
-            delete_contact(contacts, name)
+            delete_contact(contacts)
         elif choice == '5':
-            save_contact(contacts, filename)
-            print("Contact saved successfully. Exiting...")
             break
         else:
-            print("Invalid choice. Please try again")
+            print("Invalid choice. Try again!")
+
+
 if __name__ == "__main__":
-        main()
+    main()
+
+   
+        
